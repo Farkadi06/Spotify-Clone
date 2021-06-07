3b8731037aa783285087c015995a8a3b5ba097db
FARKADI OUSSAMA <58217000+Farkadi06@users.noreply.github.com>
2021-06-07 15:21:52 +0100

    Finishing the Footer UI

--- src/Componenets/Footer.js
+++ src/Componenets/Footer.js
@@ -1,17 +1,46 @@
 import React from 'react'
 import './Footer.css'
+import PlayCircleOutlineOutlinedIcon from '@material-ui/icons/PlayCircleOutlineOutlined';
+import SkipPreviousOutlinedIcon from '@material-ui/icons/SkipPreviousOutlined';
+import SkipNextOutlinedIcon from '@material-ui/icons/SkipNextOutlined';
+import ShuffleOutlinedIcon from '@material-ui/icons/ShuffleOutlined';
+import LoopOutlinedIcon from '@material-ui/icons/LoopOutlined';
+import { Grid, Slider } from '@material-ui/core';
+import PlaylistPlayOutlinedIcon from '@material-ui/icons/PlaylistPlayOutlined';
+import VolumeDownOutlinedIcon from '@material-ui/icons/VolumeDownOutlined';
 
 function Footer() {
     return (
         <div className="footer">
             <div className="footer__left">
-
+                <img 
+                    className="footer__albumLogo"
+                    src="http://wasabi-files.lbstatic.nu/files/users/large/5750587_open-uri20160525-19436-rvedgz.jpg?1464210142" 
+                    alt="" />
+                <div className="footer__songInfo">
+                    <h4>Bi*** form Pakistan waraaap</h4>
+                    <p>Houssam Riad</p>
+                </div>
             </div>
             <div className="footer__center">
-
-            </div>
+                <ShuffleOutlinedIcon className="footer__green"/>
+                <SkipPreviousOutlinedIcon className="footer__icon"/>
+                <PlayCircleOutlineOutlinedIcon fontSize="large" className="footer_icon" />
+                <SkipNextOutlinedIcon className="footer__icon"/>
+                <ShuffleOutlinedIcon className="footer__green"/>
+             </div>
             <div className="footer__right">
-                
+            <Grid container spacing={2}>
+                <Grid item>
+                    <PlaylistPlayOutlinedIcon />
+                </Grid>
+                <Grid item>
+                    <VolumeDownOutlinedIcon />
+                </Grid>
+                <Grid item xs>
+                    <Slider aria-labelledby="continuous-slider" />
+                </Grid>
+            </Grid>
             </div>
         </div>
     )
