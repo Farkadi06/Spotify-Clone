fbe34653856c1d8a22cbe89f5d50b1feed15bfdf
FARKADI OUSSAMA <58217000+Farkadi06@users.noreply.github.com>
2021-06-07 11:32:40 +0100

    Fininshing the SideBarOption

--- src/Componenets/Sidebar.js
+++ src/Componenets/Sidebar.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import './Sidebar.css'
 import SidebarOptions from './SidebarOptions'
+import HomeIcon from '@material-ui/icons/Home';
+import SearchIcon from '@material-ui/icons/Search';
+import PlayCircleFilledIcon from '@material-ui/icons/PlayCircleFilled';
+import LibraryMusicIcon from '@material-ui/icons/LibraryMusic';
+import QueueMusicIcon from '@material-ui/icons/QueueMusic';
 
 function Sidebar() {
     return (
@@ -9,9 +14,11 @@ function Sidebar() {
                 className="sidebar__logo"
                 src="https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_CMYK_White.png" 
                 alt="" />
-            <SidebarOptions title = "Home"/>
-            <SidebarOptions title = "Search"/>
-            <SidebarOptions title = "Your Libray"/>
+            <SidebarOptions Icon={HomeIcon} title = "Home"/>
+            <SidebarOptions Icon={SearchIcon} title = "Search"/>
+            <SidebarOptions Icon={LibraryMusicIcon} title = "Your Libray"/>
+            <strong>PLAY</strong>
+            <hr />
             
         </div>
     )
